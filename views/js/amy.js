8fbdd93bfd14706794bba8b7980b88f7777bc8f9
purbon <pere.urbon@gmail.com>
2013-06-25 16:27:01 +0200

    hide all resource methods by default

--- views/js/amy.js
+++ views/js/amy.js
@@ -17,24 +17,24 @@ function toggle(element) {
       var resources = Object.keys(data['resources']);
       for(var i=0; i < resources.length; i++) {
           var resource = data['resources'][resources[i]];
-          add_resource(resources[i], resource);
+          add_resource(resources[i], resource, i);
+          $('#resource'+i).toggle();
       }
-      console.log(data);
    });
  });
 
-function add_resource(resource, config) {
+function add_resource(resource, config, i) {
   var html  = '<div class="resource">';
       html += '<div class="name">  <a href="#">'+resource+'</a> </div>';
       html += '<div class="title">'+config['title']+'</div>';
       html += '<div class="menu"> <a href="#">Show/Hide</a> | <a href="#">Raw</a> </div>';
       html += '</div>';
-      html += add_section(config['sections'][0]['entries']);
+      html += add_section(resource, config['sections'][0]['entries'], i);
     $('.resources').append(html);
 }
 
-function add_section(config) {
-  var html  = '<div class="methods">';
+function add_section(resource, config, i) {
+  var html  = '<div class="methods" id="resource'+i+'">';
   var methods = Object.keys(config);
   for(var i=0; i < methods.length; i++) {
         html += '<div class="method">';
