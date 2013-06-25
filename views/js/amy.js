130cf8f27d721996f6e70c06bad9e9a7d906600d
purbon <pere.urbon@gmail.com>
2013-06-25 15:28:59 +0200

    move resources to something auto generated throw a remote json file

--- views/js/amy.js
+++ views/js/amy.js
@@ -13,9 +13,21 @@ function toggle(element) {
  }
 
  $(document).ready(function() {
-
    $.getJSON('data.json', function(data) {
-     console.log(data);
+      var resources = Object.keys(data['resources']);
+      for(var i=0; i < resources.length; i++) {
+          var resource = data['resources'][resources[i]];
+          add_resource(resources[i], resource);
+      }
+      console.log(data);
    });
-  
  });
+
+function add_resource(resource, config) {
+  var html  = '<div class="resource">';
+      html += '<div class="name">  <a href="#">'+resource+'</a> </div>';
+      html += '<div class="title">'+config['title']+'</div>';
+      html += '<div class="menu"> <a href="#">Show/Hide</a> | <a href="#">Raw</a> </div>';
+      html += '</div>';
+    $('.resources').append(html);
+}
