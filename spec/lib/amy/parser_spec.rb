25b11ddb9a28466e1a99fea19812a412b6123c86
Pere Urbon-Bayes <pere.urbon@gmail.com>
2014-05-13 22:08:04 +0200

    add method to check code switch

--- spec/lib/amy/parser_spec.rb
+++ spec/lib/amy/parser_spec.rb
@@ -17,10 +17,21 @@ describe Amy::Proc do
       expect {  parser.load_specs('spec/fitures') }.to raise_error(ArgumentError)
     end 
   
-  end
+    describe 'load_specs#source_code' do
 
-  describe "" do
-    
+      let(:parser) { Amy::Proc.new }
+      
+      before(:each) do
+        Amy::Proc.any_instance.stub(load_options_file: {'mode' => 'code'})
+      end
+
+      it "should access the source code to be loaded" do
+        expect(parser).to receive(:parse_source_code).and_return 'source_code_spec'
+        spec   = parser.load_specs dir
+        spec['resources'].should eq('source_code_spec')
+      end
+
+    end
   end
 
 end
